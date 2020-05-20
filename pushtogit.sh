8df47aa4143886a3b61162b64823947ae26fc0c7
sumqwerty <sumit999saini@gmail.com>
2020-05-20 17:50:12 +0530

    Script

--- pushtogit.sh
+++ pushtogit.sh
@@ -2,6 +2,32 @@
 
 #to clone and push changes in repo, need login because this script uses SSH key
 
+Help()
+{
+	echo "Colning of github repos and pushing the files and folders to local repos in one command"
+	echo ""
+	echo "Syntax: pushtogit.sh [-l | -r | -f | -c | -s | -k | -h]"
+	echo "options:"
+	echo "l       SSH key or HTTPS link of the repo to be cloned"
+	echo "r       name of the repo folder if its already cloned"
+	echo "c       comment to be added while performing commit. Default: 'Program Files'"
+	echo "f       file or folder name to be added or removed"
+	echo "s       add to add the file or filder to the repo and del to remove the file or folder from the repo"
+	echo "k       keep the after pushing the changs or delete it. yes for keeping it and no for deleting the local repo after pushing"
+	echo "h       display help"
+	exit 0
+}
+
+
+while getopts ":h" option; do
+	case $option in
+		h) Help #calling Help function
+		   exit;;
+
+	esac
+done
+
+
 while getopts l:f:c:s:k:r: option;do
 case "${option}" in
 l) link=${OPTARG};; #repoLink
@@ -71,5 +97,3 @@ if [ $KeepOrNot == "no" ];then
 	rm -rf $repoFolder
 	echo "Clear"
 fi
-
-echo "Done"
